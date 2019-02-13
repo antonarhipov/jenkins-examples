f5644324738310008afff21a3411a2610d68fcee
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-13 17:12:31 +0200

    Update parallel-in-sequential.groovy

--- parallel-in-sequential.groovy
+++ parallel-in-sequential.groovy
@@ -1,34 +1,34 @@
 pipeline {
     agent any
     stages {
-        stage('Non-Sequential Stage') {
+        stage("Non-Sequential Stage") {
             steps {
                 echo "On Non-Sequential Stage"
             }
         }
-        stage('Sequential') {
+        stage("Sequential") {
             environment {
                 FOR_SEQUENTIAL = "some-value"
             }
             stages {
-                stage('In Sequential 1') {
+                stage("In Sequential 1") {
                     steps {
                         echo "In Sequential 1"
                     }
                 }
-                stage('In Sequential 2') {
+                stage("In Sequential 2") {
                     steps {
                         echo "In Sequential 2"
                     }
                 }
-                stage('Parallel In Sequential') {
+                stage("Parallel In Sequential") {
                     parallel {
-                        stage('In Parallel 1') {
+                        stage("In Parallel 1") {
                             steps {
                                 echo "In Parallel 1"
                             }
                         }
-                        stage('In Parallel 2') {
+                        stage("In Parallel 2") {
                             steps {
                                 echo "In Parallel 2"
                             }
