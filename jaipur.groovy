90ff08796319d60e976f4a608eb5e57dc9fc3f7e
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-13 19:14:14 +0200

    blah

--- jaipur.groovy
+++ jaipur.groovy
@@ -1,50 +1,163 @@
 pipeline {
     agent any
     stages {
-        stage('Compile') {
+        stage('Start') {
             steps {
-                echo "Compile"
+                echo "Hello 1"
             }
         }
-        stage('Integration Tests') {
-                parallel {
-                    for (int i = 0; i < 10; i++) {
-                        stage("Integration Tests / Stage ${i}") {
-                            steps {
-                                echo 'Integration testing ${i}'
-                            }
-                        }
+
+        stage('TeamCity Parallel Stage / First Time') {
+            parallel {
+                stage('TeamCity Parallel Stage 1') {
+                    steps {
+                        echo "TC P Stage 1"
                     }
                 }
-        }
-        stage('Packaging') {
-                parallel {
-                    for (int i = 0; i < 5; i++) {
-                        stage("Packaing / Stage ${i}") {
-                            steps {
-                                echo 'Packaing ${i}'
-                            }
-                        }
+                stage('TeamCity Parallel Stage 2') {
+                    steps {
+                        echo "TC P Stage 1"
                     }
                 }
-        }
-        stage('More testing') {
-                parallel {
-                    int x = 20
-                    for (int i = 0; i < x; i++) {
-                        stage("More Testing Stuff / Stage ${i} of ${x}") {
-                            steps {
-                                echo 'More testing ${i}'
-                            }
-                        }
+                stage('TeamCity Parallel Stage 3') {
+                    steps {
+                        echo "TC P Stage 1"
                     }
                 }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploy'
+                stage('TeamCity Parallel Stage 4') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 5') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 6') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
             }
         }
+
+        stage('TeamCity Stage 2') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+        stage('TeamCity Stage 2') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Parallel Stage Second') {
+            parallel {
+                stage('TeamCity Parallel Stage Second 1') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage Second 2 - with nesting') {
+                    parallel {
+                        stage('TeamCity Parallel Nested Stage 1') {
+                            steps {
+                                echo "TC P Stage 1"
+                            }
+                        }
+                        stage('TeamCity Parallel Nested Stage 2') {
+                            steps {
+                                echo "TC P Stage 1"
+                            }
+                        }
+                        stage('TeamCity Parallel Nested Stage 3 - mwahaha') {
+                            parallel {
+                                stage('TeamCity Parallel Nested Stage 1 - mwahaha') {
+                                    steps {
+                                        echo "TC P Stage 1"
+                                    }
+                                }
+                                stage('TeamCity Parallel Nested Stage 2 - mwahaha') {
+                                    steps {
+                                        echo "TC P Stage 1"
+                                    }
+                                }
+                                stage('TeamCity Parallel Nested Stage 3 - mwahaha') {
+                                    steps {
+                                        echo "TC P Stage 1"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                stage('TeamCity Parallel Stage Second 3') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+            }
+        }
+
+
+        stage('TeamCity Stage 4') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 5') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 6') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 7') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 8') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 8') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+
+        stage('TeamCity Stage 9') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
+        stage('TeamCity Stage 10') {
+            steps {
+                echo "TC Stage 1"
+            }
+        }
+
 
     }
 }
