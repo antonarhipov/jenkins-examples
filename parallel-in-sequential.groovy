6d589676b43370f72d4c70934e5589bae58587d1
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-13 17:20:02 +0200

    Update parallel-in-sequential.groovy

--- parallel-in-sequential.groovy
+++ parallel-in-sequential.groovy
@@ -1,43 +1,17 @@
 pipeline {
-    agent none 
+    agent any 
     stages {
-        stage('Stage 1') {
-            agent { docker 'maven:3-alpine' } 
+        stage('Example Build') {
             steps {
                 echo 'Hello, Maven'
             }
         }
         stage('Example Test') {
-            agent { docker 'openjdk:8-jre' } 
-            stages {
-                stage('In Sequential 1') {
-                    steps {
-                        echo "In Sequential 1"
-                    }
-                }
-                stage('In Sequential 2') {
-                    steps {
-                        echo "In Sequential 2"
-                    }
-                }
-                stage('Parallel In Sequential') {
-                    parallel {
-                        stage('In Parallel 1') {
-                            steps {
-                                echo "In Parallel 1"
-                            }
-                        }
-                        stage('In Parallel 2') {
-                            steps {
-                                echo "In Parallel 2"
-                            }
-                        }
-                    }
-                }
+            steps {
+                echo 'Hello, JDK'
             }
         }
     }
 }
-
 
 
