d77cda155990a19f226249eb876cbb3741778c3d
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-13 17:29:00 +0200

    Update parallel-in-sequential.groovy

--- parallel-in-sequential.groovy
+++ parallel-in-sequential.groovy
@@ -6,19 +6,33 @@ pipeline {
                 echo 'Hello, Maven'
             }
         }
-        stages {
-          stage('Stage 2') {
-            stage('Stage 2-1') {
-                steps {
-                    echo 'Hello, 2-1'
+        stage('Stage 2') {
+            stages {
+                stage('In Sequential 1') {
+                    steps {
+                        echo "In Sequential 1"
+                    }
+                }
+                stage('In Sequential 2') {
+                    steps {
+                        echo "In Sequential 2"
+                    }
+                }
+                stage('Parallel In Sequential') {
+                    parallel {
+                        stage('In Parallel 1') {
+                            steps {
+                                echo "In Parallel 1"
+                            }
+                        }
+                        stage('In Parallel 2') {
+                            steps {
+                                echo "In Parallel 2"
+                            }
+                        }
+                    }
                 }
             }
-            stage('Stage 2-2') {
-                steps {
-                    echo 'Hello, 2-2'
-                }
-            }
-          }
         }
     }
 }
