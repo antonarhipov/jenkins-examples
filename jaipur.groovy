b6a8ee0402427f315618646a42f7d94c360967b0
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-14 10:30:17 +0200

    blah

--- jaipur.groovy
+++ jaipur.groovy
@@ -39,6 +39,90 @@ pipeline {
                         echo "TC P Stage 1"
                     }
                 }
+                stage('TeamCity Parallel Stage 7') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 8') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 9') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 10') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 11') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 12') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                } stage('TeamCity Parallel Stage 13') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 14') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 15') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 16') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 17') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 18') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 19') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 20') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 21') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 22') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
+                stage('TeamCity Parallel Stage 23') {
+                    steps {
+                        echo "TC P Stage 1"
+                    }
+                }
             }
         }
 
@@ -59,43 +143,59 @@ pipeline {
             parallel {
                 stage('TeamCity Parallel Stage Second 1') {
                     stages {
-                        stage('TeamCity Parallel Stage Second 1 - blah 1') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 2') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 3') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 4') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 5') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 6') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 7') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 8') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 9') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 1 - blah 10') {steps { echo '' }}
+                        stage('TeamCity Parallel Stage Second 1 - blah 1') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 2') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 3') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 4') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 5') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 6') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 7') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 8') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 9') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 1 - blah 10') { steps { echo '' } }
                     }
                 }
                 stage('TeamCity Parallel Stage Second 2') {
                     stages {
-                        stage('TeamCity Parallel Stage Second 2 - mlah 1') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 2') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 3') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 4') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 5') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 6') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 7') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 8') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 9') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 10') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 2 - mlah 11') {steps { echo '' }}
+                        stage('TeamCity Parallel Stage Second 2 - mlah 1') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 2') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 3') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 4') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 5') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 6') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 7') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 8') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 9') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 10') { steps { echo '' } }
+                        stage('TeamCity Parallel Stage Second 2 - mlah 11') { steps { echo '' } }
                     }
                 }
                 stage('TeamCity Parallel Stage Second 3') {
                     stages {
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  1') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  2') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  3') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  4') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  5') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  6') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  7') {steps { echo '' }}
-                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  8') {steps { echo '' }}
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  1') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  2') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  3') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  4') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  5') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  6') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  7') {
+                            steps { echo '' }
+                        }
+                        stage('TeamCity Parallel Stage Second 3 - quuxblasdfasdffasdfdf asdfdfsdfdiuweriyusdf sqiwieruyqweiorqyiweryoqw  8') {
+                            steps { echo '' }
+                        }
                     }
                 }
             }
