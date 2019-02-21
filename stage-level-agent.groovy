5b1aba729da2425fcd01639d2a2a7616735a6d40
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-21 14:37:33 +0100

    experimenting with nodes

--- stage-level-agent.groovy
+++ stage-level-agent.groovy
@@ -1,57 +1,59 @@
 pipeline {
     agent none 
     stages {
-        stage('Example Build') {
-            agent {
-                docker {
+        parallel {
+            stage('Example Build') {
+                agent {
+                    docker {
 //                    label 'third'
-                    image 'maven:3-alpine'
-                }
-            }
-            stages {
-                stage('Example-1'){
-                    steps {
-                        echo 'Hello, Maven'
-                        sh 'mvn --version'
+                        image 'maven:3-alpine'
                     }
                 }
-                stage('Example-2'){
-                    steps {
-                        sh 'echo AAA >> file.txt'
-                        stash includes: 'file.txt', name: 'app'
+                stages {
+                    stage('Example-1') {
+                        steps {
+                            echo 'Hello, Maven'
+                            sh 'mvn --version'
+                        }
+                    }
+                    stage('Example-2') {
+                        steps {
+                            sh 'echo AAA >> file.txt'
+                            stash includes: 'file.txt', name: 'app'
+                        }
                     }
                 }
             }
-        }
-        stage('Example Test') {
-            agent {
-                docker {
+            stage('Example Test') {
+                agent {
+                    docker {
 //                    label 'secondary'
-                    image 'openjdk:8-jre'
-                }
-            }
-            stages {
-                stage('Example-3'){
-                    steps {
-                        echo 'Hello, JDK'
-                        sh 'java -version'
+                        image 'openjdk:8-jre'
                     }
                 }
-                stage('Example-4'){
-                    steps {
-                        unstash 'app'
-                        sh 'echo BBB >> file.txt'
+                stages {
+                    stage('Example-3') {
+                        steps {
+                            echo 'Hello, JDK'
+                            sh 'java -version'
+                        }
+                    }
+                    stage('Example-4') {
+                        steps {
+                            unstash 'app'
+                            sh 'echo BBB >> file.txt'
+                        }
+                    }
+                    stage('Example-5') {
+                        steps {
+                            sh 'echo CCC >> file.txt'
+                        }
                     }
                 }
-                stage('Example-5'){
-                    steps {
-                        sh 'echo CCC >> file.txt'
+                post {
+                    always {
+                        archiveArtifacts artifacts: '*.txt', fingerprint: true
                     }
-                }
-            }
-            post {
-                always {
-                    archiveArtifacts artifacts: '*.txt', fingerprint: true
                 }
             }
         }
