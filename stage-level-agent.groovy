e380cab790ad9700e61a0e4ae3c9a6176b888828
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-21 14:41:24 +0100

    experimenting with nodes

--- stage-level-agent.groovy
+++ stage-level-agent.groovy
@@ -1,61 +1,22 @@
 pipeline {
-    agent none 
+    agent any
     stages {
         parallel {
             stage('Example Build') {
-                agent {
-                    docker {
-//                    label 'third'
-                        image 'maven:3-alpine'
-                    }
-                }
-                stages {
-                    stage('Example-1') {
-                        steps {
-                            echo 'Hello, Maven'
-                            sh 'mvn --version'
-                        }
-                    }
-                    stage('Example-2') {
-                        steps {
-                            sh 'echo AAA >> file.txt'
-                            stash includes: 'file.txt', name: 'app'
-                        }
-                    }
+                steps {
+                    sh 'echo AAA >> file.txt'
                 }
             }
             stage('Example Test') {
-                agent {
-                    docker {
-//                    label 'secondary'
-                        image 'openjdk:8-jre'
-                    }
-                }
-                stages {
-                    stage('Example-3') {
-                        steps {
-                            echo 'Hello, JDK'
-                            sh 'java -version'
-                        }
-                    }
-                    stage('Example-4') {
-                        steps {
-                            unstash 'app'
-                            sh 'echo BBB >> file.txt'
-                        }
-                    }
-                    stage('Example-5') {
-                        steps {
-                            sh 'echo CCC >> file.txt'
-                        }
-                    }
-                }
-                post {
-                    always {
-                        archiveArtifacts artifacts: '*.txt', fingerprint: true
-                    }
+                steps {
+                    sh 'echo BBB >> file.txt'
                 }
             }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: '*.txt', fingerprint: true
         }
     }
 }
