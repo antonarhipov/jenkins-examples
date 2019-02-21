031279e3cc79f56ef8977a0c9f959bdf084736a6
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-21 14:45:34 +0100

    experimenting with nodes

--- stage-level-agent.groovy
+++ stage-level-agent.groovy
@@ -1,24 +1,59 @@
 pipeline {
-    agent any
+    agent none
     stages {
-        stage("Main") {
-            parallel {
-                stage('Example Build') {
+        stage('Example Build') {
+            agent {
+                docker {
+//                    label 'third'
+                    image 'maven:3-alpine'
+                }
+            }
+            stages {
+                stage('Example-1'){
                     steps {
-                        sh 'echo AAA >> file.txt'
+                        echo 'Hello, Maven'
+                        sh 'mvn --version'
                     }
                 }
-                stage('Example Test') {
+                stage('Example-2'){
                     steps {
-                        sh 'echo BBB >> file.txt'
+                        sh 'echo AAA >> file.txt'
+                        stash includes: 'file.txt', name: 'app'
                     }
                 }
             }
         }
-    }
-    post {
-        always {
-            archiveArtifacts artifacts: '*.txt', fingerprint: true
+        stage('Example Test') {
+            agent {
+                docker {
+//                    label 'secondary'
+                    image 'openjdk:8-jre'
+                }
+            }
+            stages {
+                stage('Example-3'){
+                    steps {
+                        echo 'Hello, JDK'
+                        sh 'java -version'
+                    }
+                }
+                stage('Example-4'){
+                    steps {
+                        unstash 'app'
+                        sh 'echo BBB >> file.txt'
+                    }
+                }
+                stage('Example-5'){
+                    steps {
+                        sh 'echo CCC >> file.txt'
+                    }
+                }
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: '*.txt', fingerprint: true
+                }
+            }
         }
     }
 }
